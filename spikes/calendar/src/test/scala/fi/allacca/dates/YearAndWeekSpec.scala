99bf43589d61b68cb446ffa6ab576e517491edce
Timo Rantalaiho <timo.rantalaiho@reaktor.fi>
2014-02-05 23:59:08 +0200

    All tests together

--- spikes/calendar/src/test/scala/fi/allacca/dates/YearAndWeekSpec.scala
+++ spikes/calendar/src/test/scala/fi/allacca/dates/YearAndWeekSpec.scala
@@ -6,19 +6,10 @@ import org.scalacheck.Prop.forAll
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
+import org.scalatest.prop.Checkers
 
 @RunWith(classOf[JUnitRunner])
-class YearAndWeekSpec extends FunSpec with Matchers {
-  describe("YearOfWeek") {
-    it ("should work around start of a year") {
-      val lastWeekOf2013 = YearAndWeek(2013, 52)
-      lastWeekOf2013.next should be(YearAndWeek(2014, 1))
-      lastWeekOf2013.next.next should be(YearAndWeek(2014, 2))
-    }
-  }
-}
-
-object YearAndWeekSpecification extends Properties(classOf[YearAndWeek].getSimpleName) {
+class YearAndWeekSpec extends FunSpec with Matchers with Checkers {
   private val yawGenerator = for {
     year <- Gen.choose(1970, 2038)
     week <- Gen.choose(1,53)
@@ -26,9 +17,20 @@ object YearAndWeekSpecification extends Properties(classOf[YearAndWeek].getSimpl
 
   implicit val arbitraryYaw = Arbitrary(yawGenerator)
 
-  property("isOrderedByYearThenWeek") = forAll { (yaws: List[YearAndWeek]) => isSorted(yaws.sorted) }
+  describe("YearOfWeek") {
+    it ("should be ordered by year then week") {
+      check(forAll { (yaws: List[YearAndWeek]) => isSorted(yaws.sorted) })
+    }
+    it ("should have next week after current week") {
+      check(forAll { (current: YearAndWeek) => YearAndWeek.YearAndWeekOrdering.gt(current.next, current) })
+    }
 
-  property("nextIsAfterCurrent") = forAll { (current: YearAndWeek) => YearAndWeek.YearAndWeekOrdering.gt(current.next, current) }
+    it ("should work around start of a year") {
+      val lastWeekOf2013 = YearAndWeek(2013, 52)
+      lastWeekOf2013.next should be(YearAndWeek(2014, 1))
+      lastWeekOf2013.next.next should be(YearAndWeek(2014, 2))
+    }
+  }
 
   private def isSorted(l: List[YearAndWeek]) = {
     if (l.size < 2) true
