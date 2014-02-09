d6885a39fde11eac2e4eea9c2383852b192e484c
Timo Rantalaiho <timo.rantalaiho@reaktor.fi>
2014-02-09 22:55:39 +0200

    First go at async events loading

--- src/main/scala/fi/allacca/WeeksAdapter.scala
+++ src/main/scala/fi/allacca/WeeksAdapter.scala
@@ -10,6 +10,11 @@ import android.graphics.Color
 import android.provider.CalendarContract
 import android.database.Cursor
 import android.app.Activity
+import android.os.AsyncTask
+import android.util.Log
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 class WeeksAdapter(activity: Activity, dimensions: ScreenParameters) extends BaseAdapter {
   private val beginningOfEpoch = new DateTime(0, 1, 1, 0, 0, 0).withTimeAtStartOfDay()
@@ -18,54 +23,63 @@ class WeeksAdapter(activity: Activity, dimensions: ScreenParameters) extends Bas
 
   override def getCount: Int = Integer.MAX_VALUE
 
-  override def getItem(position: Int): (YearAndWeek, Seq[CalendarEvent]) = {
-    val week = YearAndWeek.from(beginningOfEpoch.plusWeeks(position))
-    val eventsOfWeek = loadEventsOf(week)
-    (week, eventsOfWeek)
-  }
+  override def getItem(position: Int): YearAndWeek = YearAndWeek.from(beginningOfEpoch.plusWeeks(position))
 
-  override def getItemId(position: Int): Long = getItem(position).hashCode
+  override def getItemId(position: Int): Long = position
 
   override def getView(position: Int, convertView: View, parent: ViewGroup): View = {
+    Log.d(TAG, "Get view of position " + position)
+    if (convertView != null) {
+      Log.d(TAG, "Returning convertView of position " + position)
+      return convertView
+    }
+
+    Log.d(TAG, "Creating new view of position " + position)
     val wholeLineLayout : LinearLayout = new LinearLayout(activity)
     wholeLineLayout.setOrientation(LinearLayout.HORIZONTAL)
 
-    val weekAndEvents = getItem(position)
-    val yearAndWeek = weekAndEvents._1
-    val eventsOfWeek = weekAndEvents._2
-    val days = yearAndWeek.days
-
-    var id = 1
-    val dayViews = days.map { d =>
-      val hasEvents = eventsOfWeek.exists { _.isDuring(d) }
-      val dayView = new TextView(activity)
-      dayView.setPadding(5, 5, 5, 5)
-      val fmt = DateTimeFormat.forPattern("d")
-      val dayNumber = fmt.print(d)
-      dayView.setText(dayNumber)
-      dayView.setTextColor(Color.WHITE)
-      dayView.setBackgroundColor(if (hasEvents) dimensions.governorBay else Color.BLACK)
-      dayView.setTextSize(dimensions.overviewContentTextSize)
-      dayView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL)
-      dayView.setHeight(dimensions.weekRowHeight)
-      dayView.setWidth(dimensions.dayColumnWidth)
-
-      dayView.setId(id)
-      id = id + 1
-      dayView
+    val yearAndWeek = getItem(position)
+    val weeksEventsFuture = Future { loadEventsOf(yearAndWeek) }
+    weeksEventsFuture.onComplete {
+      case Success(events) => render(events)
+      case Failure(t) => Log.e(TAG, "Error when loading", t)
     }
 
-    val weekNumberView = new TextView(activity)
-    weekNumberView.setId(id)
-    weekNumberView.setWidth(dimensions.weekNumberWidth)
-    weekNumberView.setHeight(dimensions.weekRowHeight)
-    weekNumberView.setText(yearAndWeek.week.toString)
-    weekNumberView.setTextSize(dimensions.overviewContentTextSize)
-    weekNumberView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL)
-    wholeLineLayout.addView(weekNumberView)
+    def render(eventsOfWeek: Seq[CalendarEvent]): Unit = {
+      Log.d(TAG, "Rendering " + eventsOfWeek.size)
+      val days = yearAndWeek.days
 
-    dayViews.foreach { dayView => wholeLineLayout.addView(dayView) }
+      var id = 1
+      val dayViews = days.map { d =>
+        val hasEvents = eventsOfWeek.exists { _.isDuring(d) }
+        val dayView = new TextView(activity)
+        dayView.setPadding(5, 5, 5, 5)
+        val fmt = DateTimeFormat.forPattern("d")
+        val dayNumber = fmt.print(d)
+        dayView.setText(dayNumber)
+        dayView.setTextColor(Color.WHITE)
+        dayView.setBackgroundColor(if (hasEvents) dimensions.governorBay else Color.BLACK)
+        dayView.setTextSize(dimensions.overviewContentTextSize)
+        dayView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL)
+        dayView.setHeight(dimensions.weekRowHeight)
+        dayView.setWidth(dimensions.dayColumnWidth)
 
+        dayView.setId(id)
+        id = id + 1
+        dayView
+      }
+
+      val weekNumberView = new TextView(activity)
+      weekNumberView.setId(id)
+      weekNumberView.setWidth(dimensions.weekNumberWidth)
+      weekNumberView.setHeight(dimensions.weekRowHeight)
+      weekNumberView.setText(yearAndWeek.week.toString)
+      weekNumberView.setTextSize(dimensions.overviewContentTextSize)
+      weekNumberView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL)
+      wholeLineLayout.addView(weekNumberView)
+
+      dayViews.foreach { dayView => wholeLineLayout.addView(dayView) }
+    }
     wholeLineLayout.getRootView
   }
 
