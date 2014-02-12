a3ac2e96913b083789b51ca56b479dc7ca7731b8
Perttu Auramo <perttu.auramo@gmail.com>
2014-02-12 21:55:16 +0200

    Clean up with some explicit imports

--- src/main/scala/fi/allacca/EditEventActivity.scala
+++ src/main/scala/fi/allacca/EditEventActivity.scala
@@ -4,8 +4,13 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.{TypedValue, Log}
 import android.widget._
-import android.view.ViewGroup.LayoutParams
-import android.text.{InputFilter, InputType}
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.text.InputFilter
+import android.text.InputType.TYPE_CLASS_NUMBER
+import android.text.InputType.TYPE_CLASS_TEXT
+import android.widget.RelativeLayout.BELOW
+import android.widget.RelativeLayout.RIGHT_OF
 import fi.allacca.ui.util.TextChangeListener.func2TextChangeListener
 import android.graphics.Color
 
@@ -16,42 +21,42 @@ class EditEventActivity extends Activity with TypedViewHolder {
     Log.d(TAG, "EditEventActivity.onCreate")
 
     val editLayout = new RelativeLayout(this)
-    val mainLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+    val mainLayoutParams = new RelativeLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
     editLayout.setPadding(dip2px(16), 0, dip2px(16), 0)
     editLayout.setLayoutParams(mainLayoutParams)
 
     val header = new TextView(this)
     header.setId(idGenerator.nextId)
     header.setText("Event name")
-    val params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+    val params = new RelativeLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
     header.setLayoutParams(params)
     editLayout.addView(header)
 
     val eventNameField = new EditText(this)
     eventNameField.setId(idGenerator.nextId)
-    val eventNameLayoutParams: RelativeLayout.LayoutParams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-    eventNameLayoutParams.addRule(RelativeLayout.BELOW, header.getId)
+    val eventNameLayoutParams: RelativeLayout.LayoutParams = new RelativeLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+    eventNameLayoutParams.addRule(BELOW, header.getId)
 
     eventNameField.setLayoutParams(eventNameLayoutParams)
     eventNameField.setHint("Event title")
-    eventNameField.setInputType(InputType.TYPE_CLASS_TEXT)
+    eventNameField.setInputType(TYPE_CLASS_TEXT)
     editLayout.addView(eventNameField)
 
-    val dayField = addTextField(50, 2, "d", InputType.TYPE_CLASS_NUMBER, (RelativeLayout.BELOW, eventNameField.getId))
+    val dayField = addTextField(50, 2, "d", TYPE_CLASS_NUMBER, (BELOW, eventNameField.getId))
     editLayout.addView(dayField)
 
     dayField.addTextChangedListener({ x:String => Log.i(TAG, s"Got string $x"); dayField.setTextColor(Color.RED) })
 
-    val monthField = addTextField(50, 2, "m", InputType.TYPE_CLASS_NUMBER, (RelativeLayout.BELOW, eventNameField.getId), (RelativeLayout.RIGHT_OF, dayField.getId))
+    val monthField = addTextField(50, 2, "m", TYPE_CLASS_NUMBER, (BELOW, eventNameField.getId), (RIGHT_OF, dayField.getId))
     editLayout.addView(monthField)
 
-    val yearField = addTextField(65, 4, "year", InputType.TYPE_CLASS_NUMBER, (RelativeLayout.BELOW, eventNameField.getId), (RelativeLayout.RIGHT_OF, monthField.getId))
+    val yearField = addTextField(65, 4, "year", TYPE_CLASS_NUMBER, (BELOW, eventNameField.getId), (RIGHT_OF, monthField.getId))
     editLayout.addView(yearField)
 
-    val hourField = addTextField(50, 2, "h", InputType.TYPE_CLASS_NUMBER, (RelativeLayout.BELOW, eventNameField.getId), (RelativeLayout.RIGHT_OF, yearField.getId))
+    val hourField = addTextField(50, 2, "h", TYPE_CLASS_NUMBER, (BELOW, eventNameField.getId), (RIGHT_OF, yearField.getId))
     editLayout.addView(hourField)
 
-    val minuteField = addTextField(50, 2, "m", InputType.TYPE_CLASS_NUMBER, (RelativeLayout.BELOW, eventNameField.getId), (RelativeLayout.RIGHT_OF, hourField.getId))
+    val minuteField = addTextField(50, 2, "m", TYPE_CLASS_NUMBER, (BELOW, eventNameField.getId), (RIGHT_OF, hourField.getId))
     editLayout.addView(minuteField)
 
     setContentView(editLayout)
@@ -60,7 +65,7 @@ class EditEventActivity extends Activity with TypedViewHolder {
   private def addTextField(widthDip: Float, inputLength: Int, hint: String, inputType: Int, layoutParamRules: (Int, Int)*) : EditText = {
     val textField = new EditText(this)
     textField.setId(idGenerator.nextId)
-    val textFieldParams = new RelativeLayout.LayoutParams(dip2px(widthDip), LayoutParams.WRAP_CONTENT)
+    val textFieldParams = new RelativeLayout.LayoutParams(dip2px(widthDip), WRAP_CONTENT)
     layoutParamRules.foreach { rule =>
       textFieldParams.addRule(rule._1, rule._2)
     }
