03d9294c1921d908e5ce8fc2573fd70ce19ef85c
Timo Rantalaiho <timo.rantalaiho@reaktor.fi>
2014-03-30 18:00:40 +0300

    Prepend timestamp to debug log message

--- src/main/scala/fi/allacca/Logger.scala
+++ src/main/scala/fi/allacca/Logger.scala
@@ -4,21 +4,25 @@ import android.util.Log
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import java.util.concurrent.atomic.AtomicBoolean
+import org.joda.time.format.DateTimeFormat
 
 object Logger {
   val debug = new AtomicBoolean(true)
   val TAG = "ALLACCA"
+  private val preciseTimestampFormat = DateTimeFormat.forPattern("yyyy-d-M HH:mm:ss.SSS")
 
   def info(msg: => String) {
     if (debug.get()) Log.i(TAG, msg)
   }
 
   def debug(msg: => String) {
-    if (debug.get()) Log.d(TAG, msg)
+    if (debug.get()) Log.d(TAG, createTimeStamp + " " + msg)
   }
 
   def setDebugFlag(context: Context) {
     def isDebuggable = 0 != (context.getApplicationInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE)
     debug.set(isDebuggable)
   }
+
+  private def createTimeStamp: String = preciseTimestampFormat.print(System.currentTimeMillis())
 }
